158dcb49feabd09c4068d7de108857d45d2fcd7b
lin <lhddeveloper@163.com>
2017-07-17 21:20:07 +0800

    添加菜单

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -22,4 +22,38 @@ class WechatController extends Controller
 
         return $wechat->server->serve();
     }
+
+    public function addMenu()
+    {
+        $app = app('wechat');
+        $menu = $app->menu;
+        $buttons = [
+            [
+                "type" => "click",
+                "name" => "今日歌曲",
+                "key"  => "V1001_TODAY_MUSIC"
+            ],
+            [
+                "name"       => "菜单",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "搜索",
+                        "url"  => "http://www.soso.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "视频",
+                        "url"  => "http://v.qq.com/"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "赞一下我们",
+                        "key" => "V1001_GOOD"
+                    ],
+                ],
+            ],
+        ];
+        $menu->add($buttons);
+    }
 }
